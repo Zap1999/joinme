0dc117577872799138d7d52dd3eac542b1baf5bd
Andrew Vashchenok <avashche@cisco.com>
2019-11-27 23:23:19 +0200

    hello resp changed

--- src/main/java/com/zapcorp/joinme/hello/HelloWorldController.java
+++ src/main/java/com/zapcorp/joinme/hello/HelloWorldController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,7 +17,7 @@ public class HelloWorldController {
 
     @GetMapping
     public ResponseEntity<?> hello() {
-        return ResponseEntity.ok("Hello, wassup?");
+        return ResponseEntity.ok("Hello!?");
     }
 
     @PostMapping
